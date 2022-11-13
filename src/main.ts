fb0e69993736e091edcd57773b4728c890ccb599
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-13 09:10:11 +0100

    chore: :wrench: add main file

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Movies Api')
+    .setDescription('API catolog movies')
+    .setVersion('1.0')
+    .addTag('app')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  await app.listen(3000);
+}
+
+bootstrap();
