275bf488a69e386506233679d83e6f3ac2fecde6
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-14 12:27:33 +0100

    refactor

--- src/infra/database/typeorm/data-source.ts
+++ src/infra/database/typeorm/data-source.ts
@@ -9,8 +9,8 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  entities: ['dist/src/modules/**/entities/*.{ts,js}`'],
-  migrations: ['dist/src/infra/database/typeorm/migrations/*.{ts,js}`'],
+  entities: ['dist/**/entities/*.{ts,js}'],
+  migrations: ['dist/**/migrations/*.{ts,js}'],
 };
 
 const dataSource = new DataSource(dataSourceOptions);
