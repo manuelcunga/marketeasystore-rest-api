7d1d434b56402228943d2d0c7c3949f083c85915
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-14 12:27:21 +0100

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/accounts/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { OrdersModule } from './modules/orders/orders.module';
 import { ProductModule } from './modules/products/products.module';
 import { dataSourceOptions } from './infra/database/typeorm/data-source';
-import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
   imports: [
