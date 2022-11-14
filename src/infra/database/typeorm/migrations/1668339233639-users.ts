ea25de922d78cf8739505cf2824e7be4287d8980
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-14 12:27:43 +0100

    add migrations

---
+++ src/infra/database/typeorm/migrations/1668339233639-users.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class users1668339233639 implements MigrationInterface {
+  name = 'users1668339233639';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar(120)',
+          },
+          {
+            name: 'email',
+            type: 'varchar(120)',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
