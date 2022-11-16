0e745022ae4e8aba0ec462a04bbb6c4d9a5cb2d7
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-16 13:07:50 +0100

    update

---
+++ src/infra/database/typeorm/migrations/1668431452025-orders.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class orders1668431452025 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'total_price',
+            type: 'varchar',
+          },
+          {
+            name: 'product',
+            type: 'varchar',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
+}
