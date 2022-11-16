13cf935f97d96026c3618f34a18807d78f1f7711
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-16 13:07:22 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/accounts/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { OrdersModule } from './modules/orders/orders.module';
 import { ProductModule } from './modules/products/products.module';
 import { dataSourceOptions } from './infra/database/typeorm/data-source';
+import { OrderModule } from './modules/order/order.module';
 
 @Module({
   imports: [
@@ -12,7 +12,7 @@ import { dataSourceOptions } from './infra/database/typeorm/data-source';
     TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
     ProductModule,
-    OrdersModule,
+    OrderModule,
   ],
   providers: [],
 })
