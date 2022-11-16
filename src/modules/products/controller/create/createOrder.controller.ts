56906ef6031c5a0c00b8ff4e4fb5f57328407f3b
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-16 13:11:47 +0100

    update

---
+++ src/modules/products/controller/create/createOrder.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateProducts } from '../../dtos/createProductsDTO';
+import { CreateProductService } from '../../services/create/createProduct';
+
+@UseGuards(AuthGuard('jwt'))
+@Controller('product')
+export class CreateProductController {
+  constructor(private readonly createProductService: CreateProductService) {}
+
+  @Post('/')
+  @UsePipes(ValidationPipe)
+  @HttpCode(HttpStatus.CREATED)
+  async handle(@Body() data: CreateProducts) {
+    return await this.createProductService.execute(data);
+  }
+}
