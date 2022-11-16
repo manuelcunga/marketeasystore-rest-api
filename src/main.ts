51f62c54c404c151659665fde88343ff01cc6a02
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-16 13:07:26 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
+
   const options = new DocumentBuilder()
     .setTitle('MARKETEASYSTORE Api')
     .setDescription('API MARKETEASYSTORE')
