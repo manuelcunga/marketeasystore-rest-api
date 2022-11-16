8546d6790d70ff2f178a2fbbb52f638a46badf82
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-16 13:12:41 +0100

    update

---
+++ src/modules/products/services/update/updateService.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ProductsRepository } from 'src/infra/database/typeorm/repositories/products/productsRepositories';
+import { UpdateProductsDTO } from '../../dtos/updateProductsDTO';
+import { Products } from '../../entities/products';
+
+@Injectable()
+export class UpdateProductService {
+  constructor(private productRepository: ProductsRepository) {}
+
+  async execute(id: string, data: UpdateProductsDTO): Promise<Products> {
+    const product = await this.productRepository.update(id, data);
+
+    if (!product) {
+      throw new NotFoundException(`Produto não encontrado!`);
+    }
+
+    return product;
+  }
+}
